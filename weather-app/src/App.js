503ee7910d0759c73be707d0313da2db4502ec06
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-08-08 01:27:57 +0100

    Saving code from local

---
+++ weather-app/src/App.js
@@ -0,0 +1,56 @@
+import WeatherHome from "./components/WeatherHomeScreen";
+import "./components/styles/style.css";
+import { useEffect, useState } from "react";
+// import { query } from "express";
+
+function App() {
+    //   const [weather, setWeather] = useState([]);
+    const [search, setSearch] = useState(null); // this can go directly in the form for the search bar
+    const [currentLocation, setCurrentLocation] = useState("");
+    const [currentCity, setCurrentCity] = useState("");
+  
+    const api = {
+      key: "cd51bfb08430241168a813edfe9e9d8f",
+      base: "https://api.openweathermap.org/data/2.5/",
+    };
+  
+    // when the user types a new location, you use setLocation to store the state and present it to the user
+    // when user presses enter or clicks the search button you call setCurrentLocation
+  
+    useEffect(() => {
+      //   if currentLocation is not empty -> then do a fetch to the API
+      // console.log(search);
+      if (currentCity !== "") {
+        // fetch
+        const url = `${api.base}weather?q=${currentCity}&units=metric&APPID=${api.key}`;
+        console.log(url);
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            setSearch(data);
+            //   setSearch("");
+          });
+      }
+    }, [currentCity]);
+  
+    console.log(currentCity);
+    console.log(search);
+  return (
+    <>
+      <WeatherHome
+        // weather={weather}
+        // setWeather={setWeather}
+        search={search}
+        // setSearch={setSearch}
+        // SearchWeatherLocation={SearchWeatherLocation}
+        currentLocation={currentLocation}
+        setCurrentLocation={setCurrentLocation}
+        // currentCity={currentCity}
+        setCurrentCity={setCurrentCity}
+      />
+      <main></main>
+    </>
+  );
+}
+
+export default App;
---
+++ weather-app/src/components/WeatherHomeScreen.js
@@ -0,0 +1,107 @@
+import "./styles/style.css";
+
+function WeatherHome(props) {
+  const {
+    // weather,
+    search,
+    // setSearch,
+    // SearchWeatherLocation,
+    currentLocation,
+    setCurrentLocation,
+    // currentCity,
+    setCurrentCity,
+  } = props;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCurrentCity(currentLocation);
+  }
+
+  console.log(search);
+
+  let searchData = null;
+
+  if (search !== null) {
+    searchData = (
+      <>
+        <div className="card">
+          <div className="location-box">
+            <li className="location">
+              Location: {search.name}, {search.sys.country}
+            </li>
+          </div>
+            <br/>
+          <div className="location-box">
+            <li className="location">Weather: {search.weather[0].main}</li>
+            <br/>
+            <li className="location">
+              Weather Description: {search.weather[0].description}
+            </li>
+            <br/>
+            <li className="location">Temperature: {search.main.temp}</li>
+            <br/>
+            <li className="location">
+              Max Temperature: {search.main.temp_max}
+            </li>
+            <br/>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  //   function coverChange() {
+  //     if (typeof search === null) {
+  //       return "cover default";
+  //     } else if (typeof search !== null) {
+  //       if (search.main > 10) {
+  //         return "cover warm";
+  //       } else return "cover";
+  //     }
+  //   }
+
+  //   let cover = coverChange();
+
+  return (
+    <header>
+      <div
+        className=//     ? search.main.temp > 40 //   typeof search === null // {
+        //       ? "cover warm"
+        //       : "cover"
+        //     : "cover"
+        //   coverChange()
+        //   cover
+        {search === null ? "cover default" : "cover"}
+      >
+        <div className="space-box">
+          <h1> The Mr.Weather App </h1>
+        </div>
+        <div className="space-box">
+          <h2>Today's Weather</h2>
+        </div>
+        <div className="container">
+          <form onSubmit={handleSubmit}>
+            <div className="space-box">
+              <input
+                type="text"
+                placeholder="Find Your City"
+                className="search-bar"
+                onChange={(searchEvent) =>
+                  setCurrentLocation(searchEvent.target.value)
+                }
+                value={currentLocation}
+                // onKeyPress={SearchWeatherLocation(search)}
+              />
+            </div>
+            <button type="submit">SUBMIT</button>
+          </form>
+        </div>
+        <div>
+          <ul className="cards">{searchData}</ul>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default WeatherHome;
